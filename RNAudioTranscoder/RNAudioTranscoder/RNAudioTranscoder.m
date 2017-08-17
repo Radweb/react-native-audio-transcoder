b2661063d95fe3c9557b964c62587e958aaa3494
Louis Capitanchik <contact@louiscap.co>
2017-08-17 16:43:56 +0100

    Change iOS version to convert to m4a

--- RNAudioTranscoder/RNAudioTranscoder/RNAudioTranscoder.m
+++ RNAudioTranscoder/RNAudioTranscoder/RNAudioTranscoder.m
@@ -17,53 +17,48 @@ RCT_EXPORT_METHOD(
     NSURL *inputURL = [[NSURL alloc] initFileURLWithPath:inputPath];
     NSString *outputPath = obj[@"output"];
     NSURL *outputURL = [[NSURL alloc] initFileURLWithPath:outputPath];
-    
+
     NSError *rgError = nil;
-    NSRegularExpression *replacer = [NSRegularExpression regularExpressionWithPattern:@"\\.mp3$"
+    NSRegularExpression *replacer = [NSRegularExpression regularExpressionWithPattern:@"\\.m4a$"
                                                                               options:NSRegularExpressionCaseInsensitive
                                                                                 error:&rgError];
-    
+
     NSString *tempPath = [replacer stringByReplacingMatchesInString:outputPath
                                                             options:0
                                                               range:NSMakeRange(0, outputPath.length)
-                                                       withTemplate:@""];
-    
+                                                       withTemplate:@".m4a"];
+
     if (rgError != nil) {
         reject(@"Failed to create temp path for output", rgError.localizedDescription, rgError);
         return;
     }
-    
+
     NSURL *tempUrl = [[NSURL alloc] initFileURLWithPath:tempPath];
-    
+
     AVMutableComposition *input = [[AVMutableComposition alloc] init];
     AVURLAsset *track = [AVURLAsset assetWithURL:inputURL];
-    
+
     CMTime start = kCMTimeZero;
     NSError *error = nil;
-    
+
     BOOL success = [input insertTimeRange:CMTimeRangeMake(start, track.duration) ofAsset:track atTime:start error:&error];
-    
+
     if (!success) {
         reject(@"Setup failed", error.localizedDescription, error);
         return;
     }
-    
-    AVAssetExportSession *outputSession = [[AVAssetExportSession alloc] initWithAsset:input presetName:AVAssetExportPresetHighestQuality];
+
+    AVAssetExportSession *outputSession = [[AVAssetExportSession alloc] initWithAsset:input presetName:AVAssetExportPresetPassthrough];
     outputSession.metadata = input.metadata;
     outputSession.outputURL = tempUrl;
-    outputSession.outputFileType = AVFileTypeMPEG4;
-    
+    outputSession.outputFileType = AVFileTypeAppleM4A;
+
     [outputSession exportAsynchronouslyWithCompletionHandler:^{
-        
+
         if (outputSession.status == AVAssetExportSessionStatusCompleted)
         {
-            NSError *renameError = nil;
-            [[NSFileManager defaultManager] moveItemAtPath:tempPath toPath:outputPath error:&renameError];
-            if (renameError != nil) {
-                reject(@"Failed to move file", renameError.localizedDescription, renameError);
-            } else {
-                resolve(@"Successfully encoded audio");
-            }
+
+            resolve(@"Successfully encoded audio");
         }
         else if (outputSession.status == AVAssetExportSessionStatusCancelled)
         {
